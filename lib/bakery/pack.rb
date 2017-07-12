fed39d989ed50bc40585546dc33307f2159ba232
Brett Clark <brettc@xperior.com>
2017-07-12 15:41:29 +1000

    Added rdoc.

--- lib/bakery/pack.rb
+++ lib/bakery/pack.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 module Bakery
+  ##
+  # This class represents a pack of baked goods.
   class Pack
+    attr_reader :code, :price, :quantity
+
+    ##
+    # Creates a new pack with +code+, +price+ and +quantity+.
+    def initialize(args)
+      @code = args[:code]
+      @price = args[:price]
+      @quantity = args[:quantity]
+    end
   end
 end
