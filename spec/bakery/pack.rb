9f7ece48a890f78c944bc266e2ae10d170a109d1
Brett Clark <brettc@xperior.com>
2017-07-12 15:03:45 +1000

    Add pack spec bones.

---
+++ spec/bakery/pack.rb
@@ -0,0 +1,21 @@
+RSpec.describe Bakery::Pack do
+  let(:pack) { build :pack }
+
+  describe '.code' do
+    it "returns the code" do
+      expect(pack.code).to eq 'CODE'
+    end
+  end
+
+  describe '.quantity' do
+    it "returns the quantity" do
+      expect(pack.quantity).to eq 5
+    end
+  end
+
+  describe '.price' do
+    it "returns the price" do
+      expect(pack.price).to eq 9.99
+    end
+  end
+end
---
+++ spec/factories/pack.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :pack do
+    code 'CODE'
+    quantity 5
+    price 9.99
+  end
+end
