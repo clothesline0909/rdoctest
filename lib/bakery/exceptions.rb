9f6b1d93b181767ce80c587ddf0b56403c0b1fc3
Brett Clark <clarkbab@gmail.com>
2017-07-15 09:52:28 +1000

    Shortened oversized lines.

--- lib/bakery/exceptions.rb
+++ lib/bakery/exceptions.rb
@@ -2,17 +2,17 @@
 
 module Bakery
   ##
-  # The exception is raised when the OrderParser#parse method is passed an invalid
-  # filepath.
+  # The exception is raised when the OrderParser#parse method is passed an
+  # invalid filepath.
   class InvalidFilepath < StandardError; end
 
   ##
-  # This exception is raised when the OrderParser#parse method is given an order
-  # with an incorrect format.
+  # This exception is raised when the OrderParser#parse method is given an
+  # order with an incorrect format.
   class InvalidOrderFormat < StandardError; end
 
   ##
-  # This exception is raised when the PackFactory#build method is passed an invalid
-  # array of packs' configuration.
+  # This exception is raised when the PackFactory#build method is passed an
+  # invalid array of packs' configuration.
   class InvalidPackConfig < StandardError; end
 end
